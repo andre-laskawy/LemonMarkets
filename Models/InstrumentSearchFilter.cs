56c706fc3bdf221c8b23ba0170cc7f732ccff501
Felix <fliese@gmail.com>
2021-09-05 13:31:02 +0200

    instrument search by FilterModel

---
+++ Models/InstrumentSearchFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace LemonMarkets.Models
+{
+    public class InstrumentSearchFilter
+    {
+        public string SearchByString { get; set; }
+        public List<string> SearchByIsins { get; set; }
+        public InstrumentType? InstrumentType { get; set; }
+        public TradingVenueEnum? TradingVenue { get; set; }
+        public CurrencyEnum? Currency { get; set; }
+        public bool? IsTradable { get; set; }
+        public bool WithPaging { get; set; }
+
+        public enum CurrencyEnum
+        {
+            EUR = 1,
+            USD = 2
+        }
+
+        public enum TradingVenueEnum
+        {
+            XMUN = 1
+        }
+    }
+}
