2dd93b29c0f833de4c2ad6f74a2c888c80b4083f
Felix <fliese@gmail.com>
2021-09-11 18:52:40 +0200

    Orders: json dt property converter

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,21 +1,30 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using LemonMarkets.Helper;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace LemonMarkets.Models
 {
         public class Order
-        {
+    {
         [JsonProperty("instrument")]  public InstrumentShort Instrument { get; set; }
-        [JsonProperty("valid_until")] public string ValidUntil { get; set; }
+        
+        [JsonConverter(typeof(DoubleDateTimeJsonConverter))]
+        [JsonProperty("valid_until")] public DateTime ValidUntil { get; set; }
         [JsonProperty("side")] public string Side { get; set; }
         [JsonProperty("quantity")] public int Quantity { get; set; }
         [JsonProperty("stop_price")] public string StopPrice { get; set; }
         [JsonProperty("limit_price")] public string LimitPrice { get; set; }
         [JsonProperty("uuid")] public string Uuid { get; set; }
         [JsonProperty("status")] public string Status { get; set; }
-        [JsonProperty("average_price")] public string AveragePrice { get; set; }
-        [JsonProperty("created_at")] public string CreatedAt { get; set; }
+        [JsonProperty("average_price")] public double AveragePrice { get; set; }
+        
+        [JsonConverter(typeof(DoubleDateTimeJsonConverter))]
+        [JsonProperty("created_at")] public DateTime CreatedAt { get; set; }
         [JsonProperty("type")] public string Type { get; set; }
-        [JsonProperty("processed_at")] public string ProcessedAt { get; set; }
+        
+        [JsonConverter(typeof(DoubleDateTimeJsonConverter))]
+        [JsonProperty("processed_at")] public DateTime ProcessedAt { get; set; }
         [JsonProperty("processed_quantity")] public int ProcessedQuantity { get; set; }
         }
 
